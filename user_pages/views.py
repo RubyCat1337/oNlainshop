6a8eeb690ce4d4197939093583ad8f84ce8f0028
Max_White_魚 <103893102+RubyCat1337@users.noreply.github.com>
2023-06-02 21:39:19 +0300

    Delete user_pages directory

--- user_pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def show_about_us(request):
-    return render(request, "user_pages/about_us.html")
-
-def show_main(request):
-    return render(request, "user_pages/main.html")
